177fb15620c75d44aa60992529b122a7c5847502
Yasmim de Castro <Yas.castr0.r@gmail.com>
2025-03-10 10:00:53 -0300

    Finaliza Programa

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("FATEC Rubens Lara");
+Thread.Sleep(1000);
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.WriteLine("\n Av. Senador Feijó, 350, Santos - SP");
